f3dd7e849c83626f27b461021a87e8d7258bfab6
Vladimir Shchur <odindafna2006@rambler.ru>
2023-12-14 23:21:43 -0800

    tinybasic - 1

--- 02-tiny-basic/tasks/01-hello-world.fs
+++ 02-tiny-basic/tasks/01-hello-world.fs
@@ -2,7 +2,7 @@
 // 01 - Add GOTO and better PRINT for infinite loop fun!
 // ----------------------------------------------------------------------------
 
-// NOTE: You can run this using 'dotnet run' from the terminal. 
+// NOTE: You can run this using 'dotnet run' from the terminal.
 // If you want to run code in a different file, you will need to change
 // the 'tinybasic.fsproj' file (which references this source file now).
 
@@ -13,72 +13,79 @@ module TinyBASIC
 type Value =
   | StringValue of string
 
-type Expression = 
+type Expression =
   | Const of Value
 
-type Command = 
+type Command =
   | Print of Expression
-  | Run 
-  // NOTE: GOTO specified line number. Note that this is an integer, rather 
-  // than an expression, so you cannot calculate line number dynamically. 
+  | Run
+  // NOTE: GOTO specified line number. Note that this is an integer, rather
+  // than an expression, so you cannot calculate line number dynamically.
   // (But there are tricks to do this by direct memory access on a real C64!)
   | Goto of int
 
-type State = 
+type State =
   { Program : list<int * Command> }
 
 // ----------------------------------------------------------------------------
 // Utilities
 // ----------------------------------------------------------------------------
 
-let printValue value = 
+let printValue value =
   // TODO: Take 'value' of type 'Value', pattern match on it and print it nicely.
-  failwith "not implemented"
+  match value with
+  | StringValue s -> printf "%s" s
 
 let getLine state line =
-  // TODO: Get a line with a given number from 'state.Program' (this can fail 
+  // TODO: Get a line with a given number from 'state.Program' (this can fail
   // if the line is not there.) You need this in the 'Goto' command case below.
-  failwith "not implemented"
+  state.Program |> List.find (fun (l, _) -> l = line)
 
 // ----------------------------------------------------------------------------
 // Evaluator
 // ----------------------------------------------------------------------------
 
-let rec evalExpression expr = 
-  // TODO: Implement evaluation of expressions. The function should take 
+let rec evalExpression expr =
+  // TODO: Implement evaluation of expressions. The function should take
   // 'Expression' and return 'Value'. In this step, it is trivial :-)
-  failwith "not implemented"
+  match expr with
+  | Const value -> value
 
 let rec runCommand state (line, cmd) =
-  match cmd with 
+  match cmd with
   | Print(expr) ->
       // TODO: Evaluate the expression and print the resulting value here!
-      failwith "not implemented"
+      let value = evalExpression expr
+      printValue value
       runNextLine state line
   | Run ->
-      let first = List.head state.Program    
+      let first = List.head state.Program
       runCommand state first
   | Goto(line) ->
-      // TODO: Find the right line of the program using 'getLine' and call 
+      // TODO: Find the right line of the program using 'getLine' and call
       // 'runCommand' recursively on the found line to evaluate it.
-      failwith "not implemented"
+      let line = getLine state line
+      runCommand state line
 
-and runNextLine state line = 
+and runNextLine state line =
   // TODO: Find a program line with the number greater than 'line' and evalaute
   // it using 'runCommand' (if found) or just return 'state' (if not found).
-  failwith "not implemented"
+  let findResult = state.Program |> List.tryFind (fun (l, _) -> l > line)
+  match findResult with
+  | Some line -> runCommand state line
+  | None -> state
 
 // ----------------------------------------------------------------------------
 // Test cases
 // ----------------------------------------------------------------------------
 
-let helloOnce = 
-  { Program = [ 
+let helloOnce =
+  { Program = [
       10, Print (Const (StringValue "HELLO WORLD\n")) ] }
 
-let helloInf = 
-  { Program = [ 
-      10, Print (Const (StringValue "HELLO WORLD\n")) 
+let helloInf =
+  { Program = [
+      10, Print (Const (StringValue "HELLO WORLD\n"))
       20, Goto 10 ] }
 
 // NOTE: First try to get the following to work!
