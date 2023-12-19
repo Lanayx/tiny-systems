583fdd2ce00dfe85b2bcfc331148c0ed249d045e
Vladimir Shchur <odindafna2006@rambler.ru>
2023-12-18 17:12:31 -0800

    tinyhm-1

--- 03-tiny-hm/tasks/01-numerical-solver.fsx
+++ 03-tiny-hm/tasks/01-numerical-solver.fsx
@@ -8,43 +8,49 @@ type Number =
   | Variable of string
 
 
-// NOTE: The four functions below currently return a wrong 
+// NOTE: The four functions below currently return a wrong
 // result, but one that makes the code run. As you implement
 // them (one by one), the tests should graudally start working.
 
 
-let rec occursCheck (v:string) (n:Number) = 
+let rec occursCheck (v:string) (n:Number) =
   // TODO: Check if variable 'v' appears anywhere inside 'n'
-  false
+  match n with
+  | Zero -> false
+  | Succ n -> occursCheck v n
+  | Variable v' -> v = v'
 
 let rec substite (v:string) (subst:Number) (n:Number) =
   // TODO: Replace all occurrences of variable 'v' in the
   // number 'n' with the replacement number 'subst'
-  n
+  match n with
+  | Zero -> Zero
+  | Succ n -> Succ (substite v subst n)
+  | Variable v' -> if v = v' then subst else n
 
-let substituteConstraints (v:string) (subst:Number) (constraints:list<Number * Number>) = 
-  // TODO: Substitute 'v' for 'subst' (use 'substitute') in 
+let substituteConstraints (v:string) (subst:Number) (constraints:list<Number * Number>) =
+  // TODO: Substitute 'v' for 'subst' (use 'substitute') in
   // all numbers in all the constraints in 'constraints'
   // HINT: You can use 'List.map' to implement this.
-  constraints
+  constraints |> List.map (fun (n1, n2) -> (substite v subst n1, substite v subst n2))
 
 let substituteAll (subst:list<string * Number>) (n:Number) =
   // TODO: Perform all substitutions specified  in 'subst' on the number 'n'
   // HINT: You can use 'List.fold' to implement this. Fold has a type:
   //   ('State -> 'T -> 'State) -> 'State -> List<'T> -> 'State
-  // In this case, 'State will be the Number on which we want to apply 
+  // In this case, 'State will be the Number on which we want to apply
   // the substitutions and List<'T> will be a list of substitutions.
-  n
+  subst |> List.fold (fun n (v, subst) -> substite v subst n) n
 
-let rec solve constraints = 
-  match constraints with 
+let rec solve constraints =
+  match constraints with
   | [] -> []
   | (Succ n1, Succ n2)::constraints ->
       solve ((n1, n2)::constraints)
   | (Zero, Zero)::constraints -> solve constraints
-  | (Succ _, Zero)::_ | (Zero, Succ _)::_ -> 
+  | (Succ _, Zero)::_ | (Zero, Succ _)::_ ->
       failwith "Cannot be solved"
-  | (n, Variable v)::constraints 
+  | (n, Variable v)::constraints
   | (Variable v, n)::constraints ->
       if occursCheck v n then failwith "Cannot be solved (occurs check)"
       let constraints = substituteConstraints v n constraints
@@ -53,24 +59,24 @@ let rec solve constraints =
       (v, n)::subst
 
 // Should work: x = Zero
-solve 
+solve
   [ Succ(Variable "x"), Succ(Zero) ]
 
 // Should faild: S(Z) <> Z
-solve 
+solve
   [ Succ(Succ(Zero)), Succ(Zero) ]
 
 // Should fail: No 'x' such that S(S(x)) = S(Z)
-solve 
+solve
   [ Succ(Succ(Variable "x")), Succ(Zero) ]
 
 // Not done: Need to substitute x/Z in S(x)
-solve 
+solve
   [ Succ(Variable "x"), Succ(Zero)
     Variable "y", Succ(Variable "x") ]
 
 // Not done: Need to substitute z/Z in S(S(z))
-solve 
+solve
   [ Variable "x", Succ(Succ(Variable "z"))
     Succ(Variable "z"), Succ(Zero) ]
 
