440d02d3574ce29524921d053e9f22d8a27a339f
Vladimir Shchur <odindafna2006@rambler.ru>
2023-12-20 23:26:02 -0800

    tinyhm-5

--- 03-tiny-hm/tasks/05-data-types.fsx
+++ 03-tiny-hm/tasks/05-data-types.fsx
@@ -2,7 +2,7 @@
 // Adding simple data types
 // ----------------------------------------------------------------------------
 
-type Expression = 
+type Expression =
   | Constant of int
   | Binary of string * Expression * Expression
   | If of Expression * Expression * Expression
@@ -14,10 +14,10 @@ type Expression =
   | Tuple of Expression * Expression
   | TupleGet of bool * Expression
 
-type Type = 
+type Type =
   | TyVariable of string
-  | TyBool 
-  | TyNumber 
+  | TyBool
+  | TyNumber
   | TyList of Type
   | TyFunction of Type * Type
   // NOTE: Added type for tuples
@@ -27,21 +27,55 @@ type Type =
 // Constraint solving
 // ----------------------------------------------------------------------------
 
-let rec occursCheck vcheck ty = 
+let rec occursCheck vcheck ty =
   // TODO: Add case for 'TyTuple' (same as 'TyFunction')
-  failwith "not implemented"
+  match ty with
+  | TyVariable(v) -> v = vcheck
+  | TyBool -> false
+  | TyNumber -> false
+  | TyList(ty) -> occursCheck vcheck ty
+  | TyFunction(ty1, ty2) -> occursCheck vcheck ty1 || occursCheck vcheck ty2
+  | TyTuple(ty1, ty2) -> occursCheck vcheck ty1 || occursCheck vcheck ty2
 
-let rec substType (subst:Map<_, _>) t1 = 
+let rec substType (subst:Map<_, _>) t1 =
   // TODO: Add case for 'TyTuple' (same as 'TyFunction')
-  failwith "not implemented"
+  match t1 with
+  | TyVariable(v) ->
+    match subst |> Map.tryFind v with
+    | Some(ty) -> ty
+    | None -> t1
+  | TyBool -> TyBool
+  | TyNumber -> TyNumber
+  | TyList(ty) -> TyList(substType subst ty)
+  | TyFunction(ty1, ty2) -> TyFunction(substType subst ty1, substType subst ty2)
+  | TyTuple(ty1, ty2) -> TyTuple(substType subst ty1, substType subst ty2)
 
-let substConstrs subst cs = 
-  failwith "implemented in step 2"
- 
+let substConstrs subst cs =
+  cs |> List.map (fun (n1, n2) -> (substType subst n1, substType subst n2))
+
+let substituteAll (subst:list<string*Type>) (ty:Type) =
+  subst |> List.fold (fun ty (v, subst) -> substType (Map.empty.Add(v, subst)) ty) ty
+
 let rec solve constraints =
   // TODO: Add case for 'TyTuple' (same as 'TyFunction')
-  failwith "not implemented"
-
+  let rec solveInner vars cs =
+    match cs with
+    | [] -> []
+    | (TyNumber, TyNumber)::cs -> solveInner vars cs
+    | (TyBool, TyBool)::cs -> solveInner vars cs
+    | (TyList(ty1), TyList(ty2))::cs -> solveInner vars ((ty1, ty2)::cs)
+    | (TyFunction(ta1, tb1), TyFunction(ta2, tb2))::cs -> solveInner vars ((ta1, ta2)::(tb1, tb2)::cs)
+    | (TyTuple(ty1, ty2), TyTuple(ty3, ty4))::cs -> solveInner vars ((ty1, ty3)::(ty2, ty4)::cs)
+    | (ty, TyVariable(v))::cs
+    | (TyVariable(v), ty)::cs ->
+      if occursCheck v ty then failwith "occurs check failed"
+      let newMap = vars |> Map.add v ty
+      let cs = substConstrs newMap cs
+      let subst = solveInner newMap cs
+      let ty = substituteAll subst ty
+      (v, ty)::subst
+    | _ -> failwith "cannot solve"
+  solveInner Map.empty constraints
 
 // ----------------------------------------------------------------------------
 // Constraint generation & inference
@@ -49,39 +83,75 @@ let rec solve constraints =
 
 type TypingContext = Map<string, Type>
 
-let newTyVariable = 
+let newTyVariable =
   let mutable n = 0
   fun () -> n <- n + 1; TyVariable(sprintf "_a%d" n)
 
-let rec generate (ctx:TypingContext) e = 
-  match e with 
-  | Constant _ -> failwith "implemented in step 3"
-  | Binary("+", e1, e2) -> failwith "implemented in step 3"
-  | Binary("=", e1, e2) -> failwith "implemented in step 3"
-  | Binary(op, _, _) -> failwith "implemented in step 3"
-  | Variable v -> failwith "implemented in step 3"
-  | If(econd, etrue, efalse) -> failwith "implemented in step 3"
-  | Let(v, e1, e2) -> failwith "implemented in step 4"
-  | Lambda(v, e) -> failwith "implemented in step 4"
-  | Application(e1, e2) -> failwith "implemented in step 4"
+let rec generate (ctx:TypingContext) e =
+  match e with
+  | Constant _ ->
+      TyNumber, []
+  | Binary("*", e1, e2)
+  | Binary("+", e1, e2) ->
+      let t1, s1 = generate ctx e1
+      let t2, s2 = generate ctx e2
+      TyNumber, s1 @ s2 @ [ t1, TyNumber; t2, TyNumber ]
+  | Binary("=", e1, e2) ->
+      let t1, s1 = generate ctx e1
+      let t2, s2 = generate ctx e2
+      TyBool, s1 @ s2 @ [ t1, TyNumber; t2, TyNumber ]
+  | Binary(op, _, _) ->
+      failwithf "Binary operator '%s' not supported." op
+  | Variable v ->
+      ctx[v], []
+  | If(econd, etrue, efalse) ->
+      let t1, s1 = generate ctx econd
+      let t2, s2 = generate ctx etrue
+      let t3, s3 = generate ctx efalse
+      t2, s1 @ s2 @ s3 @ [ t1, TyBool; t2, t3 ]
+  | Let(v, e1, e2) ->
+      let t1, s1 = generate ctx e1
+      let t2, s2 = generate (ctx |> Map.add v t1) e2
+      t2, s1 @ s2
+  | Lambda(v, e) ->
+      let targ = newTyVariable()
+      let t1, s1 = generate (ctx |> Map.add v targ) e
+      TyFunction(targ, t1), s1
+  | Application(e1, e2) ->
+      let t1, s1 = generate ctx e1
+      let t2, s2 = generate ctx e2
+      let targ = newTyVariable()
+      let tret = newTyVariable()
+      tret, s1 @ s2 @ [ t1, TyFunction(targ, tret); t2, targ ]
 
   | Tuple(e1, e2) ->
       // TODO: Easy. The returned type is composed of the types of 'e1' and 'e2'.
-      failwith "not implemented"
+    let t1, s1 = generate ctx e1
+    let t2, s2 = generate ctx e2
+    TyTuple(t1, t2), s1 @ s2
 
   | TupleGet(b, e) ->
       // TODO: Trickier. The type of 'e' is some tuple, but we do not know what.
       // We need to generate two new type variables and a constraint.
-      failwith "not implemented"
+      let v1 = newTyVariable()
+      let v2 = newTyVariable()
+      match b with
+      | true ->
+          let t1, s1 = generate ctx e
+          v1, s1 @ [ t1, TyTuple(v1, v2) ]
+      | false ->
+          let t1, s1 = generate ctx e
+          v2, s1 @ [ t1, TyTuple(v1, v2) ]
 
-  
+
+
 
 // ----------------------------------------------------------------------------
 // Putting it together & test cases
 // ----------------------------------------------------------------------------
 
-let infer e = 
-  let typ, constraints = generate Map.empty e 
+let infer e =
+  let typ, constraints = generate Map.empty e
   let subst = solve constraints
   let typ = substType (Map.ofList subst) typ
   typ
@@ -97,7 +167,7 @@ TupleGet(false, etup) |> infer
 
 // Interesting case with a nested tuple ('a * ('b * 'c) -> 'a * 'b)
 // * fun x -> x#1, x#2#1
-Lambda("x", Tuple(TupleGet(true, Variable "x"), 
+Lambda("x", Tuple(TupleGet(true, Variable "x"),
   TupleGet(true, TupleGet(false, Variable "x"))))
 |> infer
 
@@ -107,8 +177,8 @@ TupleGet(true, Binary("+", Constant 1, Constant 2)) |> infer
 
 
 // Combining functions and tuples ('b -> (('b -> 'a) -> ('b * 'a)))
-// * fun x f -> (x, f x)   
-Lambda("x", Lambda("f", 
-  Tuple(Variable "x", 
+// * fun x f -> (x, f x)
+Lambda("x", Lambda("f",
+  Tuple(Variable "x",
     Application(Variable "f", Variable "x"))))
 |> infer
