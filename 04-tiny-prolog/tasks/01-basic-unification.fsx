fdcf5d9ee96be15dfd28050240d91c8fcbe7cf53
Vladimir Shchur <odindafna2006@rambler.ru>
2023-12-22 14:06:51 -0800

    tinyprolog-1

--- 04-tiny-prolog/tasks/01-basic-unification.fsx
+++ 04-tiny-prolog/tasks/01-basic-unification.fsx
@@ -2,10 +2,10 @@
 // 01 - Implementing basic unication of terms
 // ----------------------------------------------------------------------------
 
-// A term is a recursive type, which can be either an atom (a known fact 
-// like 'socrates'), a variable (to which we want to asign a term by 
-// unification), or a predicate (with predicate name and a list of arguments). 
-type Term = 
+// A term is a recursive type, which can be either an atom (a known fact
+// like 'socrates'), a variable (to which we want to asign a term by
+// unification), or a predicate (with predicate name and a list of arguments).
+type Term =
   | Atom of string
   | Variable of string
   | Predicate of string * Term list
@@ -23,48 +23,64 @@ type Program = Clause list
 // Create a clause that states a fact
 let fact p = { Head = p; Body = [] }
 
-// Create a clause that defines a rule  
+// Create a clause that defines a rule
 let rule p b = { Head = p; Body = b }
 
 // ----------------------------------------------------------------------------
 // Unification of terms and lists
 // ----------------------------------------------------------------------------
 
-let rec unifyLists l1 l2 : option<list<string * Term>> = 
-  match l1, l2 with 
-  | [], [] -> 
+let rec unifyLists l1 l2 : option<list<string * Term>> =
+  match l1, l2 with
+  | [], [] ->
       // TODO: Succeeds, but returns an empty substitution
-      failwith "not implemented"
-  | h1::t1, h2::t2 -> 
+      Some []
+  | h1::t1, h2::t2 ->
       // TODO: Unify 'h1' with 'h2' using 'unify' and
-      // 't1' with 't2' using 'unifyLists'. If both 
+      // 't1' with 't2' using 'unifyLists'. If both
       // succeed, return the generated joint substitution!
-      failwith "not implemented"
-  | _ -> 
-    // TODO: Lists cannot be unified 
-    failwith "not implemented"
-
-and unify t1 t2 : option<list<string * Term>> = 
-  match t1, t2 with 
+      let u1 = unify h1 h2
+      let u2 = unifyLists t1 t2
+      match u1, u2 with
+      | Some u1, Some u2 ->
+          Some (u1 @ u2)
+      | _ ->
+          None
   | _ ->
-      // TODO: Add all the necessary cases here!
-      // * For matching atoms, return empty substitution
-      // * For matching predicates, return the result of 'unifyLists'
-      // * For variable and any term, return a new substitution
-      // * For anything else, return None (failed to unify) 
-      failwith "not implemented"
+    // TODO: Lists cannot be unified
+    None
+
+and unify t1 t2 : option<list<string * Term>> =
+  // TODO: Add all the necessary cases here!
+  // * For matching atoms, return empty substitution
+  // * For matching predicates, return the result of 'unifyLists'
+  // * For variable and any term, return a new substitution
+  // * For anything else, return None (failed to unify)
+  match t1, t2 with
+  | Atom a1, Atom a2 ->
+      if a1 = a2 then Some [] else None
+  | Predicate (p1, l1), Predicate (p2, l2) ->
+      if p1 = p2 then unifyLists l1 l2 else None
+  | Variable v, x
+  | x, Variable v ->
+      Some [v, x]
+  | _ ->
+      None
+
+
+
 
 // ----------------------------------------------------------------------------
-// Basic unification tests 
+// Basic unification tests
 // ----------------------------------------------------------------------------
 
-// Example: human(socrates) ~ human(X) 
+// Example: human(socrates) ~ human(X)
 // Returns: [X -> socrates]
 unify
   (Predicate("human", [Atom("socrates")]))
   (Predicate("human", [Variable("X")]))
 
-// Example: human(socrates) ~ mortal(X) 
+// Example: human(socrates) ~ mortal(X)
 // Returns: None (fail)
 unify
   (Predicate("human", [Atom("socrates")]))
