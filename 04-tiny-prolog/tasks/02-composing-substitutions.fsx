933e0e2e82e9366e42fb26f600a5626deb95539c
Vladimir Shchur <odindafna2006@rambler.ru>
2023-12-23 16:03:38 -0800

    tinyprolog-2

--- 04-tiny-prolog/tasks/02-composing-substitutions.fsx
+++ 04-tiny-prolog/tasks/02-composing-substitutions.fsx
@@ -2,7 +2,7 @@
 // 02 - Composing and applying substitutions
 // ----------------------------------------------------------------------------
 
-type Term = 
+type Term =
   | Atom of string
   | Variable of string
   | Predicate of string * Term list
@@ -21,28 +21,35 @@ let rule p b = { Head = p; Body = b }
 // Substitutions and unification of terms
 // ----------------------------------------------------------------------------
 
-let rec substitute (subst:Map<string, Term>) term = 
+let rec substitute (subst:Map<string, Term>) (term: Term) =
   // TODO: Replace variables in 'term' for which there is a
   // replacement specified by 'subst.[var]' with the replacement.
   // You can assume the terms in 'subst' do not contain
   // any of the variables that we want to replace.
-  failwith "not implemented"
+  match term with
+  | Atom _ -> term
+  | Variable var ->
+    match subst.TryFind(var) with
+    | Some term -> term
+    | None -> term
+  | Predicate (name, terms) ->
+      Predicate (name, List.map (substitute subst) terms)
 
 
-let substituteSubst (newSubst:Map<string, Term>) (subst:list<string * Term>) = 
-  // TODO: Apply the substitution 'newSubst' to all the terms 
-  // in the existing substitiution 'subst'. (We represent one 
-  // as a map and the other as a list of pairs, which is a bit 
+let substituteSubst (newSubst:Map<string, Term>) (subst:list<string * Term>) =
+  // TODO: Apply the substitution 'newSubst' to all the terms
+  // in the existing substitiution 'subst'. (We represent one
+  // as a map and the other as a list of pairs, which is a bit
   // inelegant, but it makes calling this function easier later.)
-  failwith "not implemented"
+  subst |> List.map (fun (var, term) -> (var, substitute newSubst term))
 
 
-let substituteTerms (subst:Map<string, Term>) (terms:list<Term>) = 
+let substituteTerms (subst:Map<string, Term>) (terms:list<Term>) =
   // TODO: Apply substitution 'subst' to all the terms in 'terms'
-  failwith "not implemented"
+  terms |> List.map (substitute subst)
 
 
-let rec unifyLists l1 l2 = 
+let rec unifyLists l1 l2 : Option<List<string * Term>>=
   // TODO: Modify the implementation to use 'substituteTerms' and 'substituteSubst'.
   //
   // Let's say that your code calls 'unify h1 h2' to get a substitution 's1'
@@ -52,11 +59,39 @@ let rec unifyLists l1 l2 =
   // (1) The substitution 's1' is aplied to 't1' and 't2' before calling 'unifyLists'
   // (2) The substitution 's2' is applied to all terms in substitution 's1' before returning
   //
-  // You can look at your ML type inference code. The structure is very similar! 
-  failwith "implemented in step 1"
+  // You can look at your ML type inference code. The structure is very similar!
+  match l1, l2 with
+  | [], [] ->
+      Some []
+  | h1::t1, h2::t2 ->
+      let s1 = unify h1 h2
+      match s1 with
+      | None -> None
+      | Some s1 ->
+          let s1map = Map(s1)
+          let t1x = substituteTerms s1map t1
+          let t2x = substituteTerms s1map t2
+          let s2 = unifyLists t1x t2x
+          match s2 with
+          | None -> None
+          | Some s2 ->
+              let s2map = Map(s2)
+              let s1x = substituteSubst s2map s1
+              Some (s1x @ s2)
+  | _ -> None
 
-and unify t1 t2 = 
-  failwith "implemented in step 1"
+
+and unify (t1: Term) (t2: Term) :Option<List<string * Term>> =
+  match t1, t2 with
+  | Atom a1, Atom a2 ->
+      if a1 = a2 then Some [] else None
+  | Predicate (p1, l1), Predicate (p2, l2) ->
+      if p1 = p2 then unifyLists l1 l2 else None
+  | Variable v, x
+  | x, Variable v ->
+      Some [v, x]
+  | _ ->
+      None
 
 // ----------------------------------------------------------------------------
 // Advanced unification tests requiring correct substitution
@@ -94,10 +129,10 @@ unify
 // Example: add(succ(X), X) ~ add(Y, succ(Z))
 // Returns: [ X -> succ(Z); Y -> succ(succ(Z)) ]
 unify
-  (Predicate("add", 
-      [ Predicate("succ", [Variable("X")]); 
+  (Predicate("add",
+      [ Predicate("succ", [Variable("X")]);
         Variable("X") ]))
-  (Predicate("add", 
-      [ Variable("Y"); 
+  (Predicate("add",
+      [ Variable("Y");
         Predicate("succ", [Variable("Z")]) ]))
 
